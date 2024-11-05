20c6c3c18fbb73d515f246c7bade4c0704049dfc
Gabriel Melo <97925932+GabrielMeloSan@users.noreply.github.com>
2024-11-05 20:11:27 -0300

    Add files via upload

---
+++ AVL/Arvore.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+class Contato {
+    private:
+    std::string nome, email;
+    int telefone;
+    Contato* left;
+    Contato* right;
+
+    public:
+    Contato(std::string nome,std::string email, int telefone):nome(string){
+
+    }
+}
